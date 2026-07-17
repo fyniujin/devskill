8deced54c4fcfa0d524cc6b95ea29b4038ead2c2
fyniujin <56947009+fyniujin@users.noreply.github.com>
2026-07-17 21:15:36 +0800

    update: cn-model-gateway/src/adapters/__init__.py [skip ci]

--- cn-model-gateway/src/adapters/__init__.py
+++ cn-model-gateway/src/adapters/__init__.py
@@ -6,9 +6,14 @@ from .zhipu import ZhiPuAdapter
 from .kimi import KimiAdapter
 from .hunyuan import HunYuanAdapter
 from .doubao import DouBaoAdapter
+from .minimax import MiniMaxAdapter
+from .lingyi import LingYiAdapter
+from .baichuan import BaichuanAdapter
+from .stepfun import StepFunAdapter
 
 __all__ = [
     "BaseAdapter", "ChatMessage", "ChatResponse", "ContentChunk",
     "DeepSeekAdapter", "TongYiAdapter", "ZhiPuAdapter",
     "KimiAdapter", "HunYuanAdapter", "DouBaoAdapter",
+    "MiniMaxAdapter", "LingYiAdapter", "BaichuanAdapter", "StepFunAdapter",
 ]
